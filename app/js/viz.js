9cffb1e68969dbcb72ab7e40a4c1070ea08723aa
IMG-PRCSNG <samthedevil.sp@gmail.com>
2022-01-27 15:50:07 +0000

    Add functions to draw images to canvas as required by the visualisations

--- app/js/viz.js
+++ app/js/viz.js
@@ -52,7 +52,9 @@ class Visualization {
         this.tcanvas.height = this.canvas.height = im1Data.height;
         this.tctx.putImageData(this.resultData, 0, 0);
 
-        this.canvas.dispatchEvent(new CustomEvent('transform'));
+        this.canvas.dispatchEvent(new CustomEvent('transform', {
+            bubbles: true,
+        }));
     }
 
     getImageData (img) {
@@ -77,12 +79,71 @@ class Visualization {
         return this.bctx.getImageData(0, 0, ow, oh);
     }
 
-    draw_image(img) {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    draw_slide_image(slide_factor, is_horizontal = false) {
+        const rw = this.canvas.width;
+        const rh = this.canvas.height;
+
+        const iw = this.input_image.naturalWidth || this.input_image.width;
+        const ih = this.input_image.naturalHeight || this.input_image.height;
+
+        const ow = is_horizontal ? slide_factor * rw : rw;
+        const oh = is_horizontal ? rh : slide_factor * rh;
+
+        this.ctx.clearRect(0, 0, rw, rh);
+
+        if (is_horizontal) {
+          this.ctx.drawImage(this.input_image, 0, 0, iw * slide_factor, ih, 0, 0, ow, oh);
+          this.ctx.drawImage(
+            this.tcanvas,
+            ow,
+            0,
+            rw - ow,
+            oh,
+            ow,
+            0,
+            rw - ow,
+            oh
+          );
+        } else {
+          this.ctx.drawImage(this.input_image, 0, 0, iw, ih * slide_factor, 0, 0, ow, oh);
+          this.ctx.drawImage(
+            this.tcanvas,
+            0,
+            oh,
+            rw,
+            rh - oh,
+            0,
+            oh,
+            rw,
+            rh - oh
+          );
+        }
+      };
+
+    draw_image(img, clear=false) {
+        if (clear) {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
         this.ctx.drawImage(
             img, 0, 0, img.naturalWidth || img.width, img.naturalHeight || img.height,
             0, 0, this.canvas.width, this.canvas.height
         )
+    }
+
+    draw_toggle_image(flag = false) {
+        this.draw_image(flag ? this.tcanvas : this.input_image, true);
+    }
+
+    draw_diff_image() {
+        this.ctx.putImageData(this.diffData, 0, 0);
+    }
+
+    draw_overlay_image(factor) {
+        this.ctx.save();
+        this.draw_image(this.input_image, true);
+        this.ctx.globalAlpha = factor;
+        this.draw_image(this.tcanvas, false);
+        this.ctx.restore();
     }
 
     toggle_images(speed) {
@@ -100,7 +161,7 @@ class Visualization {
             if (this.TOGGLE_IMAGES) {
                 this.toggle_timer = setTimeout(cb, 1000 / speed);
             }
-            this.draw_image(toggle_flag ? this.tcanvas : this.input_image);
+            this.draw_toggle_image(toggle_flag);
             toggle_flag = !toggle_flag;
         }.bind(this));
         this.toggle_timer = setTimeout(cb, 0);
